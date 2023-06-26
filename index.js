0e5d5bcd60d200d81f2c9d5e01327e98e0a0e00a
PaschalALX <pasmac2018@gmail.com>
2023-06-26 22:26:00 +0100

    Added Task 12

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject';
+import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject'
+import createReportObject from './0x00-ES6_basic/12-createReportObject';
 
-console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};      
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
