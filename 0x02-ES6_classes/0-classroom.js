d4330afaa7008bbc8c167c0f4a2de2660ffb8bd6
PaschalALX <pasmac2018@gmail.com>
2023-06-29 14:36:03 +0100

    Refactored

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,9 +1,9 @@
-export default class ClassRoom{
-    /**
-     * 
-     * @param {Number} maxStudentsSize 
+export default class ClassRoom {
+  /**
+     *
+     * @param {Number} maxStudentsSize
      */
-    constructor(maxStudentsSize){
-        this._maxStudentsSize = maxStudentsSize
-    }
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize; /* eslint-disable-line no-underscore-dangle */
+  }
 }
