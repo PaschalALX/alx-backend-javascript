c718b6a5e19abe271655e52099bd27d2af2b83c1
PaschalALX <pasmac2018@gmail.com>
2023-06-29 20:33:54 +0100

    Added Task 5&6

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,23 +1,21 @@
 export default class Building {
-    /**
-     * 
-     * @param {Number} sqft 
+  /**
+     *
+     * @param {Number} sqft
      */
-    constructor(sqft) {
-        this._sqft = sqft
-
-        if (this.constructor !== 'Building') {
-
-            if (typeof (this.evacuationWarningMessage) !== 'function') {
-                throw new Error(
-                    'Class extending Building must override evacuationWarningMessage'
-                )
-            }
-        }
+  constructor(sqft) {
+    this._sqft = sqft; /* eslint-disable-line no-underscore-dangle */
+    const methodKeys = Object.getOwnPropertyNames(this.constructor.prototype);
+    if (methodKeys.indexOf('evacuationWarningMessage') === -1) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
+  }
 
-    get sqft() {
-        return this._sqft
-    }
+  get sqft() {
+    return this._sqft; /* eslint-disable-line no-underscore-dangle */
+  }
 
+  evacuationWarningMessage() {} /* eslint-disable-line class-methods-use-this */
 }
